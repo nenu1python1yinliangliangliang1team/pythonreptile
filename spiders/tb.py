401e25c4806336a7fae4c71d3f8c86eca6cdacd1
yinll314 <403118842@qq.com>
2017-05-22 22:04:58 +0800

    Update tb.py

--- spiders/tb.py
+++ spiders/tb.py
@@ -12,8 +12,31 @@ class TbSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-       pass
+        key="零食"
+        for i in range(0,101):
+            url="https://s.taobao.com/search?q="+str(key)+"&s="+str(44*i)
+            yield Request(url=url,callback=self.page)
     def page(self,response):
-       pass
+        body=response.body.decode("utf-8","ignore")
+        patid='"nid":"(.*?)"'
+        allid=re.compile(patid).findall(body)
+        print(allid)
+        for j in range(0,len(allid)):
+            thisid=allid[j]
+            url1="https://item.taobao.com/item.htm?id="+str(thisid)
+            yield Request(url=url1,callback=self.next)
     def next(self,response):
-        pass
+        item=TaobaoItem()
+        item["title"]=response.xpath("//h3[@class='tb-main-title']/@data-title").extract()
+        item["link"]=response.url
+        item["price"]=response.xpath("//em[@class='tb-rmb-num']/text()").extract()
+        patid='id=(.*?)$'
+        thisid=re.compile(patid).findall(response.url)[0]
+        commenturl="https://rate.taobao.com/detailCount.do?callback=jsonp100&itemId="+str(thisid)
+        #print(commenturl)
+        commentdata=urllib.request.urlopen(commenturl).read().decode("utf-8","ignore")
+        #print(commentdata)
+        pat='"count":(.*?)}'
+        item["comment"]=re.compile(pat).findall(commentdata)
+        #print(item["comment"])
+        yield item
