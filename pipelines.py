d2cde1b7a5c856decc2c93c76b2c6abf32633083
yinll314 <403118842@qq.com>
2017-05-22 21:58:41 +0800

    Update pipelines.py
    爬取数据的后续处理

--- pipelines.py
+++ pipelines.py
@@ -8,8 +8,21 @@ import pymysql
 
 class TaobaoPipeline(object):
     def __init__(self):
+        #self.conn=pymysql.connect(host="127.0.0.1",user="root",passwd="root",db="csdn")
        pass
     def process_item(self, item, spider):
-       pass
+        try:
+            title=item["title"][0]
+            link=item["link"]
+            price=item["price"][0]
+            comment=item["comment"][0]
+            print(title)
+            print(price)
+            print(comment)
+            sql="insert into taob(title,link,price,comment) values('"+title+"','"+link+"','"+price+"','"+comment+"')"
+            self.conn.query(sql)
+            return item
+        except Exception as err:
+            pass
     def close_spider(self):
-       pass
+        self.conn.close()
